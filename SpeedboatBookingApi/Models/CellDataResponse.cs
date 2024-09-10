98d4f2a5f1bda1ef530ff760f93f0f894e03764c
Ivan Dominis <ivan.dominis@emovis.hr>
2024-09-10 07:34:51 +0200

    Removed ignored files from Git tracking

--- SpeedboatBookingApi/Models/CellDataResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SpeedboatBookingApi.Models
-{
-    public class CellDataResponse
-    {
-        public string Value { get; set; }
-        public string BackgroundColor { get; set; }
-        public string? TextColor { get; set; }
-    }
-}
